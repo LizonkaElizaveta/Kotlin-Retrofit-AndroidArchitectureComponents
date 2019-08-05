72010c3ad08ecd3b371d662f0ca81bbe28687464
LizonkaElizaveta <liza_soft_1313@mail.ru>
2019-08-05 19:01:41 +0300

    create data class Hotel

---
+++ app/src/main/java/stanevich/elizaveta/hoteldisplay/Hotel.kt
@@ -0,0 +1,10 @@
+package stanevich.elizaveta.hoteldisplay
+
+data class Hotel(
+    var name: String,
+    var address: String,
+    var stars: String,
+    var distance: String,
+    var suites_availability: String
+) {
+}
