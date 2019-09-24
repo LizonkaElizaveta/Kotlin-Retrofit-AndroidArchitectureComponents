21b0b7f3f64991e452e36608a819a209fc3c5851
LizonkaElizaveta <liza_soft_1313@mail.ru>
2019-09-24 15:37:38 +0300

    delete all classes and files

--- app/src/main/java/stanevich/elizaveta/hoteldisplay/MainActivity.kt
+++ app/src/main/java/stanevich/elizaveta/hoteldisplay/MainActivity.kt
@@ -1,51 +1,13 @@
 package stanevich.elizaveta.hoteldisplay
 
 import android.os.Bundle
-import android.os.Parcelable
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.fragment_hotel_list.*
-import stanevich.elizaveta.hoteldisplay.detail.HotelDescriptionFragment
-import stanevich.elizaveta.hoteldisplay.detail.OnHotelSelected
-import stanevich.elizaveta.hoteldisplay.network.HotelProperty
-import stanevich.elizaveta.hoteldisplay.overview.HotelListFragment
 
 
-class MainActivity : AppCompatActivity(), OnHotelSelected {
-
-    override fun onHotelSelected(hotelProperty: HotelProperty) {
-        val detailsFragment =
-            HotelDescriptionFragment.newInstance(hotelProperty)
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.root_layout, detailsFragment, "hotelDetails")
-            .addToBackStack(null)
-            .commit()
-    }
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        if (savedInstanceState == null) {
-            supportFragmentManager
-                .beginTransaction()
-                .add(R.id.root_layout, HotelListFragment.newInstance(), "hotelList")
-                .commit()
-        }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
-        super.onSaveInstanceState(outState, outPersistentState)
-        outState.putParcelable(
-            "KeyForLayoutManagerState",
-            recyclerview.layoutManager?.onSaveInstanceState()
-        )
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        val state = savedInstanceState.getParcelable<Parcelable>("KeyForLayoutManagerState")
-        recyclerview.layoutManager?.onRestoreInstanceState(state)
     }
 }
--- app/src/main/java/stanevich/elizaveta/hoteldisplay/detail/OnHotelSelected.kt
+++
@@ -1,7 +0,0 @@
-package stanevich.elizaveta.hoteldisplay.detail
-
-import stanevich.elizaveta.hoteldisplay.network.HotelProperty
-
-interface OnHotelSelected {
-    fun onHotelSelected(hotelProperty: HotelProperty)
-}
